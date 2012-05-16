c69ca5ff060cfa39ffe5489d0a13225f1ce4a940
Miguel Senquiz <miguel.senquiz@gmail.com>
2012-05-15 23:14:41 -0400

    added entry delete functionality

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -24,4 +24,14 @@ class EntriesController < ApplicationController
     end
   end
 
+  def destroy
+    @entry = Entry.find(params[:id])
+    @entry.destroy
+
+    respond_to do |format|
+      format.html { redirect_to entries_url }
+      format.json { head :no_content }
+    end
+  end
+
 end
